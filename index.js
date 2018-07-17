a84dd8252037a4f4221eb0c71885c4ef2baea8a5
slowbeam <sedwardscode@gmail.com>
2018-07-16 22:00:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const logDriverNames = function (drivers) {
+  drivers.forEach(function(driver) {console.log(driver.name)}
+)};
+
+const logDriversByHometown = function (drivers, location) {
+
+  drivers.forEach(function (driver){
+    if (driver.hometown === location) {
+      console.log(driver.name)
+    }
+  });
+};
+
+const driversByRevenue = function (drivers) {
+  drivers.sort(function (a,b){
+    return a.revenue - b.revenue;
+  });
+};
